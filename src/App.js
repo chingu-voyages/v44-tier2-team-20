90fddcf12abd2bef008e2ea6595beb2b337130d8
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2023-05-14 16:12:41 -0700

    added core css and js for App component'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UserConfigBox from './components/UserConfigBox/UserConfigBox';
 
 function App() {
 	return (
 		<div>
 			<h1>React App</h1>
+			<UserConfigBox />
 		</div>
 	);
 }
