305b2bc4fdfe4906f1a03567103ec1c6ebe27030
nataliaYarr <n.yarysheva@gmail.com>
2023-05-14 18:04:43 -0700

    leaderboard_ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import LeaderBoard from './components/LeaderBoard/LeaderBoard/LeaderBoard';
 
 function App() {
 	return (
 		<div>
 			<h1>React App</h1>
-			<LeaderBoard />
 		</div>
 	);
 }
