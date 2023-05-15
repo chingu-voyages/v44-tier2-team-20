43628788ac15e3983d8aab0d212ef3e76e4fe76d
nataliaYarr <n.yarysheva@gmail.com>
2023-05-14 17:44:33 -0700

    leaderboard_design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
 import './App.css';
+<<<<<<< HEAD
 import UserConfigBox from './components/UserConfigBox/UserConfigBox';
+=======
+import LeaderBoard from './components/LeaderBoard/LeaderBoard/LeaderBoard';
+>>>>>>> 78a5e9d (leaderboard_design)
 
 function App() {
 	return (
 		<div>
 			<h1>React App</h1>
+<<<<<<< HEAD
 			<UserConfigBox />
+=======
+			<LeaderBoard />
+>>>>>>> 78a5e9d (leaderboard_design)
 		</div>
 	);
 }
