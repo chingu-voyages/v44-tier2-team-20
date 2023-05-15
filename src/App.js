0983e51388ffe65dc09b9d9fc35d6fecc889b4b7
BSMuse <bsmuse@tutamail.com>
2023-05-15 08:39:47 -0600

    Create BattleLog component and CSS. Update package.json to fix openssl issues.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import React from 'react';
+import BattleLog from './components/BattleLog.jsx'
 
 function App() {
 	return (
 		<div>
 			<h1>React App</h1>
+			<BattleLog/>
 		</div>
 	);
 }
---
+++ src/components/BattleLog.jsx
@@ -0,0 +1,68 @@
+import React, { useRef, useEffect } from "react"
+import styles from './BattleLog.module.css'
+
+function BatteLog(props){
+
+   const [logs, setLogs] = React.useState([
+      {
+         bots: ['Bot1', 'Bot2'],
+         result: 'Computations'
+      },
+      {
+         bots: ['Bot3', 'Bot2'],
+         result: 'blug blugb robots, bluh bluh won and won and won and won'
+      },
+      {
+         bots: ['Bot1', 'Bot2'],
+         result: 'Computations'
+      },
+      {
+         bots: ['Bot1', 'Bot2'],
+         result: 'Computations'
+      },
+      {
+         bots: ['Bot1', 'Bot2'],
+         result: 'Computations'
+      },
+      {
+         bots: ['Bot1', 'Bot2'],
+         result: 'Computations'
+      },
+      {
+         bots: ['Bot1', 'Bot2'],
+         result: 'Computations'
+      },
+      {
+         bots: ['Bot1', 'Bot2'],
+         result: 'Computations'
+      }
+   ])
+
+   // creating reference for wrapper to adjust scroll
+   const scrollableElement = useRef(null);
+
+   // makes the scroll bar head to bottom on first render and with every new log in array so user doesn't have to scroll down for new result
+   useEffect(() => {
+      if (scrollableElement.current) {
+         scrollableElement.current.scrollTop = scrollableElement.current.scrollHeight - scrollableElement.current.clientHeight;
+      }
+   }, [logs]);
+
+          // maps logs to wrapper
+          const renderLogs = () => {
+            return logs.map((log, index) => (
+               <div key= {index} className={styles.container}>
+                  <p className={styles.bots_txt}>{log.bots[0]} vs {log.bots[1]}</p>
+                  <p className={styles.results_txt}>{log.result}</p>
+               </div>
+            ))
+         }
+
+   return (
+    <div className={styles.wrapper} ref={scrollableElement}>
+      <p>Battle Log</p>
+         {renderLogs()}
+    </div>
+   )
+}
+export default BatteLog
