da33726d7910c1dc430bf5f697fc81cee3322b51
nataliaYarr <n.yarysheva@gmail.com>
2023-05-21 15:20:38 -0700

    App grid layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import LeaderBoard from './components/LeaderBoard/LeaderBoard/LeaderBoard';
+import UserConfigBox from './components/UserConfigBox/UserConfigBox';
+import AppGrid from './components/AppGrid/AppGrid';
+
 
 function App() {
 	return (
 		<div>
-			<h1>React App</h1>
-			<LeaderBoard />
+			{/* <h1>React App</h1> */}
+			<AppGrid />
 		</div>
 	);
 }
---
+++ src/components/AppGrid/AppGrid.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useRef, useEffect } from 'react'
+import styles from './AppGrid.module.css'
+
+const AppGrid = () => {
+  const [containerSize, setContainerSize] = useState({ height: 0, width: 0 });
+
+  const containerRef = useRef(null);
+  const squareRef = useRef(null);
+
+  useEffect(() => {
+    const reportContainerSize = () => {
+      const containerHeight = containerRef.current.offsetHeight;
+      const containerWidth = containerRef.current.offsetWidth;
+      setContainerSize({ height: containerHeight, width: containerWidth });
+    };
+
+    reportContainerSize();
+    window.addEventListener('resize', reportContainerSize);
+
+    return () => {
+      window.removeEventListener('resize', reportContainerSize);
+    };
+  }, []);
+  
+  useEffect(() => {
+      let n = Math.min(containerSize.height, containerSize.width);
+      squareRef.current.style.height = `${n}px`;
+      squareRef.current.style.width = `${n}px`;
+  });
+
+
+  return (
+    <div className={styles.container}>
+        <div ref={containerRef}  className={styles.container__arena}>
+          <div ref={squareRef}  className={styles.container__arena_square}>responsive arena that keeps 1:1 ratio</div>
+        </div>
+          {/* both take the same grid-area */}
+        <div className={styles.container__leaderboard}>LeaderBoard</div>
+          <div className={styles.container__boolean}>Boolean</div>
+        <div className={styles.container__users}>Users</div>
+        <div className={styles.container__log}>Battle log</div>
+    </div>
+  )
+}
+
+export default AppGrid
