1734c0697fabc0186a5d52ab5155f0a2dfab011f
BSMuse <bsmuse@tutamail.com>
2023-05-28 10:23:35 -0600

    Create Arena component and add speed and coordinates to bot

--- src/components/GameContext/Bot.js
+++ src/components/GameContext/Bot.js
@@ -1,12 +1,14 @@
 import { v4 as uuidv4 } from 'uuid';
 
 class Bot {
-  constructor(name, binaryValue, direction) {
+  constructor(name, binaryValue, image, direction) {
     this.id = uuidv4();
     this.name = name;
     this.image= image;
     this.binaryValue = binaryValue;
-    this.direction = direction
+    this.direction = direction;
+    this.coordinates = [row[index]];
+    this.speed = 0;
     this.wins = 0;
     this.losses = 0;
   }
@@ -40,9 +42,29 @@ class Bot {
     this.direction = direction;
   } 
 
+   // Method to set coordinates
+  setCoordinates(row,index) {
+    this.coordinates = [row[index]];
+  }
+
+  // Method to set speed 
+  setSpeed(speed) {
+    this.speed = speed;
+  }
+
   // Method to retrieve the binary value of the bot (to use in Arena)
   getBinaryValue() {
     return this.binaryValue;
+  }
+
+  // Method to get coordinates
+  getCoordinates(){
+    return this.coordinates;
+  }
+
+  // Method to get speed 
+  getSpeed(){
+    return this.speed;
   }
 
   // Method to retrieve losses of the bot (to use in Leaderboard)
