7c6193df6c3ded018602836dbfe59156d0b8a3dc
nataliaYarr <n.yarysheva@gmail.com>
2023-06-01 13:46:31 -0700

    reconfigure leaderboard

--- src/components/LeaderBoard/LeaderBoard/LeaderBoard.test.jsx
+++ src/components/LeaderBoard/LeaderBoard/LeaderBoard.test.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import LeaderBoard from './LeaderBoard';
 
 describe('LeaderBoard', () => {
-  const mockBots = [
-    {
-      name: 'Bot1',
-      wins: 11,
-      losses: 0,
-    },
-    {
-      name: 'Bot2',
-      wins: 17,
-      losses: 5,
-    },
-    {
-      name: 'Bot3',
-      wins: 1,
-      losses: 10,
-    },
-  ];
+  // Mock the GameContext
+jest.mock('../../../context/GameContext/GameContext', () => ({
+  GameContext: {
+    bots: [
+      { name: 'Bot1', wins: 1, losses: 0 },
+      { name: 'Bot2', wins: 0, losses: 1 },
+    ],
+  },
+}));
+
 
   test('renders the LeaderBoard component with correct headings', () => {
-    render(<LeaderBoard bots={mockBots} />);
+    render(<LeaderBoard />);
 
     // Assert that the "Leaders" heading is rendered
     const leadersHeading = screen.getByRole('heading', { level: 2, name: /leaders/i });
@@ -39,7 +32,7 @@ describe('LeaderBoard', () => {
   });
 
   test('renders the BotStatistics component with the correct number of bots', () => {
-    render(<LeaderBoard bots={mockBots} />);
+    render(<LeaderBoard />);
 
     // Assert that the BotStatistics component is rendered
     const botStatisticsComponent = screen.getByTestId('bot-statistics');
@@ -47,6 +40,43 @@ describe('LeaderBoard', () => {
 
     // Assert that the correct number of bots are rendered within the BotStatistics component
     const botItems = screen.getAllByTestId('bot-item');
-    expect(botItems.length).toBe(mockBots.length);
+    expect(botItems.length).toBe(bots.length);
+  });
+
+  test('adds bots to uniqueBots array and updates bot\'s wins/losses', () => {
+    render(<LeaderBoard />);
+  
+    // Mock the useState hook for bots and setBots
+    const setBots = jest.fn();
+    jest.mock('react', () => ({
+      ...jest.requireActual('react'),
+      useState: initialBots => [initialBots, setBots],
+    }));
+  
+    // Set the initial state for bots
+    act(() => {
+      setBots([
+        { name: 'Bot1', wins: 1, losses: 0 },
+        { name: 'Bot2', wins: 0, losses: 1 },
+      ]);
+    });
+  
+    expect(bots).toEqual([
+      { name: 'Bot1', wins: 1, losses: 0 },
+      { name: 'Bot2', wins: 0, losses: 1 },
+    ]);
+  
+    // Add another bot and check the updated state of bots
+    act(() => {
+      setBots(prevBots => [
+        ...prevBots,
+        { name: 'Bot2', wins: 2, losses: 1 },
+      ]);
+    });
+  
+    expect(bots).toEqual([
+      { name: 'Bot1', wins: 1, losses: 0 },
+      { name: 'Bot2', wins: 2, losses: 2 },
+    ]);
   });
 });
