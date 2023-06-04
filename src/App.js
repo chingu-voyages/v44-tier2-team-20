912e4ab6299093393739d21eea1891876edc13c8
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2023-06-04 08:44:04 -0700

    added correct spelling to BattleLog

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import { GameProvider } from './context/GameContext/GameContext';
 import UserConfigBox from './components/UserConfigBox/UserConfigBox';
-import BatteLog from './components/BattleLog/BattleLog';
+import BattleLog from './components/BattleLog/BattleLog';
 
 function App() {
 	return (
 		<div>
 			<GameProvider>
 				<UserConfigBox />
-				<BatteLog />
+				<BattleLog />
 			</GameProvider>
 		</div>
 	);
