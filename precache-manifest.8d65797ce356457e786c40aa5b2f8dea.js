07acf18741d5a0f8dd0d803c8d964ef8114f13a9
Jarrod <jarrodsavard2x180@gmail.com>
2023-06-11 20:08:08 -0700

    Updates

---
+++ precache-manifest.8d65797ce356457e786c40aa5b2f8dea.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "bedf0568d9208f4e9603",
+    "url": "/v44-tier2-team-20/static/css/main.cfb2db54.chunk.css"
+  },
+  {
+    "revision": "bedf0568d9208f4e9603",
+    "url": "/v44-tier2-team-20/static/js/main.80b6c83b.chunk.js"
+  },
+  {
+    "revision": "87e3d17530b63a755ec5",
+    "url": "/v44-tier2-team-20/static/js/runtime~main.d1515269.js"
+  },
+  {
+    "revision": "2537f4c03b4fbc8ad113",
+    "url": "/v44-tier2-team-20/static/js/2.91e82624.chunk.js"
+  },
+  {
+    "revision": "85b67d58420062fbecec",
+    "url": "/v44-tier2-team-20/static/js/3.eff08ea3.chunk.js"
+  },
+  {
+    "revision": "1c235c9c3eea9164d4c6f8331e5bd005",
+    "url": "/v44-tier2-team-20/static/media/bot-1.1c235c9c.png"
+  },
+  {
+    "revision": "ae5aa1388636dcb96c613a1a5f6f772a",
+    "url": "/v44-tier2-team-20/static/media/bot-2.ae5aa138.png"
+  },
+  {
+    "revision": "5e66f967a82426d7cc4a4d5a6f48649e",
+    "url": "/v44-tier2-team-20/static/media/bot-3.5e66f967.png"
+  },
+  {
+    "revision": "2d9326712d8d88e7910b29a41e1df304",
+    "url": "/v44-tier2-team-20/static/media/bot-4.2d932671.png"
+  },
+  {
+    "revision": "cfac91dc205b123a2669758334e7c25a",
+    "url": "/v44-tier2-team-20/static/media/bot-5.cfac91dc.png"
+  },
+  {
+    "revision": "56fbabae98997a749cfc9ceb3ae20c13",
+    "url": "/v44-tier2-team-20/static/media/bot-6.56fbabae.png"
+  },
+  {
+    "revision": "b092214134cdcd2679e1d5d55a078b4c",
+    "url": "/v44-tier2-team-20/static/media/bot-7.b0922141.png"
+  },
+  {
+    "revision": "9820ef3e359ed3199528f02d2c017b3c",
+    "url": "/v44-tier2-team-20/static/media/bot-8.9820ef3e.png"
+  },
+  {
+    "revision": "bbff64aef7909cecd2ca3ef48c4e22aa",
+    "url": "/v44-tier2-team-20/static/media/bot-9.bbff64ae.png"
+  },
+  {
+    "revision": "92b6cdea06d9838cec17da957866f5cf",
+    "url": "/v44-tier2-team-20/static/media/bot-10.92b6cdea.png"
+  },
+  {
+    "revision": "1e0e578917ef3aff755f7be6a07bcd84",
+    "url": "/v44-tier2-team-20/static/media/bot-11.1e0e5789.png"
+  },
+  {
+    "revision": "8df66667c1e76a5185b8d89c77eb2f62",
+    "url": "/v44-tier2-team-20/static/media/3.8df66667.jpg"
+  },
+  {
+    "revision": "0fbc12e3ce21fa1d3044fcd8d6cb067b",
+    "url": "/v44-tier2-team-20/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/v44-tier2-team-20/precache-manifest.8d65797ce356457e786c40aa5b2f8dea.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/v44-tier2-team-20/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
